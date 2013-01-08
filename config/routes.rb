7dc7d0e95961452c0bf25ee1ce3d9900fe459dac
Bartosz Walecki <bw@projektpartner.pl>
2013-01-08 22:03:16 +0100

    Na razie koniec z wyglądem

--- config/routes.rb
+++ config/routes.rb
@@ -61,4 +61,7 @@ AdsRb::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  match '/about', :to=>'pages#about'
+  match '/contact', :to=>'pages#contact'
+  match '/', :to=>'pages#home'
 end
