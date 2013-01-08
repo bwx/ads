bc7cca59b0a4c8deb7343f49390aa66f282f4f01
Bartosz Walecki <bw@projektpartner.pl>
2013-01-08 01:01:27 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdsRb::Application.config.secret_token = 'b27fe004f6999641d1d3b3c79bd4fd7bbf380cfc30de73bce5f1f447d08b922ef80dccfcae7c74bf5beb1c620c794f34d5f65c8c29549fcd9a87622576a3d5ad'
